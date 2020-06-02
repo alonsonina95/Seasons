2bc6c622ee5f589fc688f3f2390d93bd089f4bde
Alonso Ninalaya <pikachualonso@gmail.com>
2020-06-02 01:12:35 -0400

    Adding spinner component and seasonDisplay component as well

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Description
+Application that shows the seasons with a nice design
 
-How to find the location of the user: We are using Geolocation API
+# Libraries being used
+- How to find the location of the user: We are using Geolocation API
+- We are using semantic-ui library for icons and design 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        // When our component first show up
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
     render() {
-            if (this.state.errorMessage && !this.state.lat ) {
-                return <div>Error: {this.state.errorMessage}</div>
-            }
+        if (this.state.errorMessage && !this.state.lat ) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
 
-            if (!this.state.errorMessage && this.state.lat ) {
-            return <div>latitude: {this.state.lat}</div>
-            }
-
-            return <div>Loading!</div>
+        if (!this.state.errorMessage && this.state.lat ) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <Spinner/>;
     }
 }
 
